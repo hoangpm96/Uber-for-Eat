dd634a00b62d4bb39ae3e901175eb7926b5c5eda
Hoang Phan <hoangpm.qn96@gmail.com>
2018-09-24 02:55:16 +0700

    add get list meals api for customer

--- foodtaskerapp/apis.py
+++ foodtaskerapp/apis.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 
-from foodtaskerapp.models import Restaurant
-from foodtaskerapp.serializers import RestaurantSerializer
+from foodtaskerapp.models import Restaurant, Meal, Order, OrderDetails
+from foodtaskerapp.serializers import RestaurantSerializer, MealSerializer
 
 def customer_get_restaurants(request):
     restaurants = RestaurantSerializer(
@@ -11,3 +11,18 @@ def customer_get_restaurants(request):
     ).data
 
     return JsonResponse({"restaurants": restaurants})
+
+def customer_get_meals(request, restaurant_id):
+    meals = MealSerializer(
+        Meal.objects.filter(restaurant_id = restaurant_id).order_by("-id"),
+        many = True,
+        context = {"request": request}
+    ).data
+
+    return JsonResponse({"meals": meals})
+
+def customer_add_order(request):
+    return JsonResponse({})
+
+def customer_get_latest_order(request):
+    return JsonResponse({})
