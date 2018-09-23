31e1b0a401a11ea263881183e24cdbe0930a4ff2
Hoang Phan <hoangpm.qn96@gmail.com>
2018-09-23 17:16:03 +0700

    Create add meal and view list meals

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from foodtaskerapp.forms import UserForm, RestaurantForm, UserFormForEdit
+from foodtaskerapp.forms import UserForm, RestaurantForm, UserFormForEdit, MealForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from foodtaskerapp.models import Meal
 
 # Create your views here.
 def home(request):
@@ -32,7 +33,25 @@ def restaurant_account(request):
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_meal(request):
-    return render(request, 'restaurant/meal.html', {})
+    meals = Meal.objects.filter(restaurant = request.user.restaurant).order_by("-id")
+    return render(request, 'restaurant/meal.html', {"meals": meals})
+
+@login_required(login_url='/restaurant/sign-in/')
+def restaurant_add_meal(request):
+    form = MealForm()
+
+    if request.method == "POST":
+        form = MealForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            meal = form.save(commit=False)
+            meal.restaurant = request.user.restaurant
+            meal.save()
+            return redirect(restaurant_meal)
+
+    return render(request, 'restaurant/add-meal.html', {
+        "form": form
+    })
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_order(request):
